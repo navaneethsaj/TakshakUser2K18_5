a5a59226d642b22714a3f8598695cbca40be6565
Aravind Balakrishnan <aravindbalan222@gmail.com>
2018-07-19 21:46:17 +0530

    ui needs change

---
+++ app/src/main/java/takshak/mace/takshak2k18/MainActivity.java
@@ -0,0 +1,227 @@
+package takshak.mace.takshak2k18;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.view.InflateException;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import static takshak.mace.takshak2k18.R.color.black;
+
+@SuppressWarnings("ALL")
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, Tab1.OnFragmentInteractionListener,Tab2.OnFragmentInteractionListener,Tab3.OnFragmentInteractionListener {
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    VideoView vid;
+    String path;
+    Uri videouri;
+    String geturl="https://demo4664908.mockable.io/happy";
+    @TargetApi(Build.VERSION_CODES.M)
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setBackgroundColor(Color.rgb(255,127,80));
+        toolbar.setTitleTextColor(Color.WHITE);
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tablayout);
+        tabLayout.addTab(tabLayout.newTab().setText("Home"));
+        tabLayout.addTab(tabLayout.newTab().setText("Highlights"));
+        tabLayout.addTab(tabLayout.newTab().setText("Reach Radio"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        tabLayout.setTabTextColors(Color.rgb(205,201,201),Color.WHITE);
+        final ViewPager viewPager = (ViewPager)findViewById(R.id.viewpager);
+        final PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.setOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        fab.setRippleColor(Color.GREEN);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                String shareBody = "Hey have look at Takshak'18 official app on playstore\nhttps://play.google.com/store/apps/details?id=com.facebook.katana\n";
+                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Takshak'18 Official app");
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(sharingIntent, "Share via"));
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        return true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+
+            return true;
+        }
+        if (id == R.id.notify){
+            goto_notification();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.login) {
+            // notificatons
+            Intent i = new Intent(getApplicationContext(),profile.class);
+            startActivity(i);
+        }
+        else if (id == R.id.profile){
+           // profile
+            Intent pro = new Intent(getApplicationContext(),ProfileCardView.class);
+            startActivity(pro);
+        }
+         else if (id == R.id.about) {
+            //about
+
+        } else if (id == R.id.locate) {
+            //locate in maps
+            double latitude = 10.053952;
+            double longitude = 76.619336;
+            String label = "We are Here !";
+            String uriBegin = "geo:" + latitude + "," + longitude;
+            String query = latitude + "," + longitude + "(" + label + ")";
+            String encodedQuery = Uri.encode(query);
+            String uriString = uriBegin + "?q=" + encodedQuery + "&z=5";
+            Uri uri = Uri.parse(uriString);
+            Intent mapIntent = new Intent(android.content.Intent.ACTION_VIEW, uri);
+            startActivity(mapIntent);
+
+        } else if (id ==R.id.facebook){
+            //facebook
+            try {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("fb://page/274889042525005"));
+                startActivity(intent);
+            } catch(Exception e) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/TakshakOfficial/")));
+            }
+        } else if (id == R.id.instagram){
+            //insta
+            try {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("com.instagram.android.https://www.instagram.com/takshak18"));
+                startActivity(intent);
+            } catch(Exception e) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/takshak18")));
+            }
+        }else if (id == R.id.twitter){
+            try {
+                // get the Twitter app if possible
+                getApplicationContext().getPackageManager().getPackageInfo("com.twitter.android", 0);
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("twitter://user?screen_name=OfficialTakshak"));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            } catch (Exception e) {
+                // no Twitter app, revert to browser
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/OfficialTakshak"));
+                startActivity(intent);
+            }
+
+        }
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return false;
+    }
+    public void get(View v){
+        Toast.makeText(this,"sss",Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+    public void goto_notification(){
+        Intent iv = new Intent(getApplicationContext(),profile.class);
+        startActivity(iv);
+    }
+}
