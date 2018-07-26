aa399755a1b8f3f54226e101a178a8b6931ca786
Navaneeth <navaneethsaj@gmail.com>
2018-07-26 17:03:24 +0530

    photo upload done

--- app/src/main/java/takshak/mace/takshak2k18/photo.java
+++ app/src/main/java/takshak/mace/takshak2k18/photo.java
@@ -2,12 +2,14 @@ package takshak.mace.takshak2k18;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -19,8 +21,19 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 import static android.app.Activity.RESULT_OK;
@@ -31,18 +44,29 @@ public class photo extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private static final String IMAGECOUNT = "imagecount";
     Button click;
     Button pick;
+    Button send;
     Bitmap selectedImage;
     ImageView imageView;
     File photoFile;
-
+    public final String UserPREFERENCES = "userinfo";
+    public final String NAME = "username";
+    public final String USERID = "usserid";
+    public final String SONGREQCOUNT = "reqcount";
+    public final String MOBILENO = "mobileno";
+    public final String EMAILID = "emailid";
+    Uri imageUri;
+    private StorageReference mStorageRef;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    SharedPreferences sharedPreferences;
 
 
     private OnFragmentInteractionListener mListener;
+    private SharedPreferences.Editor editor;
 
     public photo() {
         // Required empty public constructor
@@ -73,6 +97,9 @@ public class photo extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+        sharedPreferences = getActivity().getSharedPreferences(UserPREFERENCES, Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
     }
 
     @Override
@@ -129,6 +156,7 @@ public class photo extends Fragment {
         click = view.findViewById(R.id.snap);
         pick = view.findViewById(R.id.picker);
         imageView = view.findViewById(R.id.photo);
+        send = view.findViewById(R.id.sendbutton);
         pick.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -138,6 +166,58 @@ public class photo extends Fragment {
 
             }
         });
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (imageUri != null){
+                    send.setEnabled(false);
+                    String location = sharedPreferences.getString(USERID,"userid")+"_"
+                            +sharedPreferences.getString(NAME,"name")+"_"
+                            +sharedPreferences.getString(IMAGECOUNT,"1");
+                    StorageReference riversRef = mStorageRef.child("images/"+location);
+
+                    Bitmap bitmap = null;
+                    try {
+                        bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), imageUri);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    Bitmap compressed = getResizedBitmap(bitmap,500);
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    compressed.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                    byte[] data = baos.toByteArray();
+
+                    riversRef.putBytes(data).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Toast.makeText(getContext(),"Success",Toast.LENGTH_LONG).show();
+                            send.setVisibility(View.GONE);
+                            send.setEnabled(true);
+
+                            int imageCount = Integer.parseInt(sharedPreferences.getString(IMAGECOUNT,"1"));
+                            imageCount++;
+                            editor.putString(IMAGECOUNT, String.valueOf(imageCount));
+                            editor.commit();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getContext(),"Failed",Toast.LENGTH_LONG).show();
+                            send.setEnabled(true);
+                        }
+                    }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                            progress = (int) Math.round(progress);
+                            int percent = (int) progress;
+                            Log.d("progress", String.valueOf(percent)+"%");
+                            send.setText(String.valueOf(percent)+"%");
+                        }
+                    });
+                }
+            }
+        });
 
     }
 
@@ -145,52 +225,115 @@ public class photo extends Fragment {
      public void onActivityResult(int reqCode, int resultCode, Intent data) {
 
         super.onActivityResult(reqCode, resultCode, data);
+
+
+        Log.d("reqcodevalue", String.valueOf(reqCode));
         if (reqCode==0){
+
+            Log.d("reqcode", " 0");
             if (resultCode == RESULT_OK) {
                 try {
-                    final Uri imageUri = data.getData();
+                    imageUri = data.getData();
                     ContentResolver c = getContext().getContentResolver();
                     final InputStream imageStream = c.openInputStream(imageUri);
                     selectedImage = BitmapFactory.decodeStream(imageStream);
                     imageView.setImageBitmap(selectedImage);
+                    send.setVisibility(View.VISIBLE);
+
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
                     Toast.makeText(getContext(), "Something went wrong", Toast.LENGTH_LONG).show();
-                }
+                    send.setVisibility(View.GONE);}
 
             } else {
+                send.setVisibility(View.GONE);
                 Toast.makeText(getContext(), "You haven't picked Image", Toast.LENGTH_LONG).show();
             }
         }
-        if (reqCode==1){
-            // set the dimensions of the image
-            int targetW =120;
-            int targetH = 100;
-
-            // Get the dimensions of the bitmap
-            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-            bmOptions.inJustDecodeBounds = true;
-            BitmapFactory.decodeFile(photoFile.getAbsolutePath(), bmOptions);
-            int photoW = bmOptions.outWidth;
-            int photoH = bmOptions.outHeight;
-
-            // Determine how much to scale down the image
-            int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
-
-            // Decode the image file into a Bitmap sized to fill the View
-            bmOptions.inJustDecodeBounds = false;
-            bmOptions.inSampleSize = scaleFactor;
-
-            //InputStream stream = getContentResolver().openInputStream(data.getData());
-            selectedImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
-            imageView.setImageBitmap(selectedImage);
-        }
+//        if (reqCode==1){
+//            Log.d("reqcode", " 1");
+//            // set the dimensions of the image
+//            int targetW =120;
+//            int targetH = 100;
+//
+//            // Get the dimensions of the bitmap
+//            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+//            bmOptions.inJustDecodeBounds = true;
+//            BitmapFactory.decodeFile(photoFile.getAbsolutePath(), bmOptions);
+//            int photoW = bmOptions.outWidth;
+//            int photoH = bmOptions.outHeight;
+//
+//            // Determine how much to scale down the image
+//            int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+//
+//            // Decode the image file into a Bitmap sized to fill the View
+//            bmOptions.inJustDecodeBounds = false;
+//            bmOptions.inSampleSize = scaleFactor;
+//
+//            //InputStream stream = getContentResolver().openInputStream(data.getData());
+//            selectedImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+//            imageView.setImageBitmap(selectedImage);
+//        }
 
 
 
     }
+    public Bitmap getResizedBitmap(Bitmap image, int maxSize) {
+        int width = image.getWidth();
+        int height = image.getHeight();
 
+        float bitmapRatio = (float)width / (float) height;
+        if (bitmapRatio > 1) {
+            width = maxSize;
+            height = (int) (width / bitmapRatio);
+        } else {
+            height = maxSize;
+            width = (int) (height * bitmapRatio);
+        }
+        return Bitmap.createScaledBitmap(image, width, height, true);
+    }
 
+    public File saveBitmapToFile(File file){
+        try {
 
+            // BitmapFactory options to downsize the image
+            BitmapFactory.Options o = new BitmapFactory.Options();
+            o.inJustDecodeBounds = true;
+            o.inSampleSize = 6;
+            // factor of downsizing the image
+
+            FileInputStream inputStream = new FileInputStream(file);
+            //Bitmap selectedBitmap = null;
+            BitmapFactory.decodeStream(inputStream, null, o);
+            inputStream.close();
+
+            // The new size we want to scale to
+            final int REQUIRED_SIZE=75;
+
+            // Find the correct scale value. It should be the power of 2.
+            int scale = 1;
+            while(o.outWidth / scale / 2 >= REQUIRED_SIZE &&
+                    o.outHeight / scale / 2 >= REQUIRED_SIZE) {
+                scale *= 2;
+            }
+
+            BitmapFactory.Options o2 = new BitmapFactory.Options();
+            o2.inSampleSize = scale;
+            inputStream = new FileInputStream(file);
+
+            Bitmap selectedBitmap = BitmapFactory.decodeStream(inputStream, null, o2);
+            inputStream.close();
+
+            // here i override the original image file
+            file.createNewFile();
+            FileOutputStream outputStream = new FileOutputStream(file);
+
+            selectedBitmap.compress(Bitmap.CompressFormat.JPEG, 100 , outputStream);
+
+            return file;
+        } catch (Exception e) {
+            return null;
+        }
+    }
 
 }
